7ec30441c85d41a8d7ec17bd5a87c41e04a025a3
KenichiHiroi <151005957+KenichiHiroi@users.noreply.github.com>
2023-12-14 15:42:58 +0900

    課題提出
    ・キューブが地面およびキューブ同士と衝突したときの効果音

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -10,10 +10,16 @@ public class CubeController : MonoBehaviour
     //消滅位置
     private float deadLine = -10;
 
+    //衝突音
+    public AudioClip CollisionSound;
+    private AudioSource ASComponent;
+    //衝突音を出力済みか否か
+    private bool isCollided = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ASComponent = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +32,23 @@ public class CubeController : MonoBehaviour
         if(this.transform.position.x < this.deadLine)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //このキューブが衝突音を出力していない場合、衝突オブジェクトの判定に進む
+        if(this.isCollided == false)
+        {
+            //衝突したオブジェクトがキューブか地面の場合、衝突音を再生する
+            if(collision.gameObject.tag == "CubeTag" || collision.gameObject.tag == "GroundTag")
+            {
+                //再生
+                ASComponent.PlayOneShot(CollisionSound);
+            }
+
+            //衝突音の出力済みをTrueにする
+            this.isCollided = true;
         }
     }
 }
